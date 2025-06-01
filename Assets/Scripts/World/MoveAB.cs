84ba3d01be18deed18182c9588cd11b273a97c0c
RaCuNi <thisisracuni@gmail.com>
2025-06-01 23:58:23 +0900

    Add scripts for stage world objects' moving.

---
+++ Assets/Scripts/World/MoveAB.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+public class MoveAB : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float movingSpeed = 1.0f;
+    public float waitTime = 1.0f;
+
+    private Transform targetPoint;
+    private bool isWaiting = false;
+    void Start()
+    {
+        if (pointA == null || pointB == null)
+        {
+            Debug.LogError("Point A and Point B must be assigned in the inspector.");
+            return;
+        }
+
+        targetPoint = pointB;
+    }
+    
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    void MoveToTarget()
+    {
+        if (!isWaiting)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPoint.position, movingSpeed * Time.deltaTime);
+        }
+        
+        if (Vector2.Distance(transform.position, targetPoint.position) < 0.01f)
+        {
+            if (!isWaiting)
+            {
+                StartCoroutine(WaitThenSwitchTarget());
+            }
+        }
+    }
+
+    IEnumerator WaitThenSwitchTarget()
+    {
+        // Wait at the target point
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTime);
+
+        // Switch target point
+        targetPoint = (targetPoint == pointA) ? pointB : pointA;
+        isWaiting = false;
+    }
+}
